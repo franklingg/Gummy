74734c99be5da957e3658b1c3a9f3c3e8aad1e0d
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-06-18 12:24:51 -0300

    movie command

--- src/commands/filme.js
+++ src/commands/filme.js
@@ -1,10 +1,6 @@
+const Discord = require('discord.js');
 const {NotEnoughArgs, InvalidArgs} = require("../utils/BotError");
-
-const steps = [
-    {
-        text: "Bora marcar esse filmer, digite um título e (se quiser) um subtítulo pra mensagem.\nNesse formato: TITULO | SUBTITULO",
-    }
-]
+const emojis = require("../data/emojiCharacters");
 
 module.exports = {
 	name: 'filme',
@@ -12,22 +8,47 @@ module.exports = {
 	description: 'Setup para registrar uma enquete de filmes',
 	usage: `Como usar: "${process.env.prefix}filme"`,
 	execute: (message, args) => {
-        const collector = message.channel.createMessageCollector(r=>r, { time: 30000, errors: ['time'] });
-        let step = 0;
-
-        collector.on('collect', m =>{
-            while(step < steps.length){
-                if(m.content.trim() === 'fim') {
-                    collector.stop();
-                    break;
-                }
-                const nextStep = steps[step];
-                message.channel.send(nextStep.text);
-            }
-        });
-
-        collector.on('end', collected =>{
-            message.channel.bulkDelete(collected.size, true);
+        let title, subtitle;
+        message.channel.send('Digite um título e subtítulo (se quiser) para a mensagem.\nEx: Qual filmeh hojer? | Fala logo vey');
+        message.channel.awaitMessages(m=>m, {max: 2, time: 90000, errors:['time']})
+        .then( collected =>{
+            [title, subtitle] = collected.last().content.split('|');
+            message.channel.send('Agora bota as opções ai vey. Ex: filme1 | filme2 | filme3');
+            return message.channel.awaitMessages(m=>m, {max: 2, time: 120000, errors:['time']});
         })
+        .then( collected => {
+            message.channel.bulkDelete(5);
+            const movies = collected.last().content.split('|');
+            const embedMessage = new Discord.MessageEmbed()
+                                    .setColor('#FFF654')
+                                    .setTitle(title)
+                                    .setDescription(subtitle || '')
+                                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                                    .addFields(
+                                        movies.map((movie, idx)=>{
+                                            return { name: `Opção ${idx+1}`, value: movie.trim(), inline: true}
+                                        })
+                                    );
+            message.channel.send(embedMessage).then(embedded => {                    
+                movies.forEach((movie, idx) => {
+                    embedded.react(emojis[idx+1]);
+                })
+                const movieRole = message.guild.roles.cache.find(role => role.name === process.env.MOVIE_ROLE);
+                const collector = embedded.createReactionCollector((r, u) => r);
+                collector.on('collect', (reaction, user)=>{
+                    const reactMember = message.guild.members.cache.filter(member => {
+                        console.log(member.username);
+                        return member.username === user.username
+                    }).first();
+                    if(reactMember) {
+                        console.log(reactMember);
+                        reactMember.roles.add(movieRole);
+                    }
+                });
+            });
+        })
+        .catch( collected =>{
+            message.reply('Demorou demais vey, acha que eu tenho o dia todo???');
+        });
 	},
 };
