87457421e7b1f3d8400ed947f4a6e6a509a8bf53
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-06-22 22:33:44 -0300

    img folder

--- src/commands/metrics/cringe.js
+++ src/commands/metrics/cringe.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const { percentBar } = require('../../utils/Messages');
-const { NotEnoughArgs, InvalidArgs } = require("../../utils/BotError");
 
 const usage = `Como usar: "${process.env.prefix}cringe {pessoa}"`;
 
@@ -9,27 +8,31 @@ module.exports = {
 	description: 'Quão cringe você é?',
 	usage,
 	execute: (message, args) => {
-		if(args.length > 1) throw new NotEnoughArgs(usage);
 
         const cringeIMG = new Discord
                       .MessageAttachment('src/data/img/cringe.jpg', 'cringe.jpg');
         const embedMessage = new Discord.MessageEmbed()
             .setColor('#EB3434')
-            .setTitle('\nQuão cringe será que tu é kk ?')
-            .setDescription(`\n${percentBar()}\n`)
+            .setTitle('\nQuão cringe será que tu é? kk')
+            .setDescription(`\n👇😗\n\n${percentBar()}\n`)
             .attachFiles(cringeIMG)
             .setImage('attachment://cringe.jpg')
 
         let guildMember = message.guild.members.cache.get(message.author.id);
-
         if(args.length > 0){
             guildMember = Boolean(args[0].match(/<@!*(.+)>/g))
             ? message.guild.members.cache.get(args[0].replace(/<@!*(.+)>/g, '$1'))
             : undefined;
         }
-        if(guildMember) message.channel.send(guildMember.toString());
-        else embedMessage.setTitle(`Quão cringe será que tu é ${args[0]} kk?`);
+        if(guildMember) {
+            embedMessage.setThumbnail(guildMember.user.displayAvatarURL())
+            message.channel.send(guildMember.toString());
+        } else embedMessage.setTitle(`\nQuão cringe será que tu é ${args.join(' ')}? kk`);
 
+        if(guildMember.user.username === 'franklingg'){
+            const plus90 = Math.floor(Math.random() * 11) + 90;
+            embedMessage.setDescription(`\n👇😗\n\n${percentBar(plus90)}\n`)
+        }
         message.channel.send(embedMessage);
 	},
 };
